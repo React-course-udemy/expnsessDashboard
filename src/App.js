c2483be917f305497451be2327a210b207155e02
thepassant <bassant.ayman69@gmail.com>
2021-12-26 10:37:55 +0200

    add object to use es6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
+const App = () => {
+  const expenses =[{title: 'car insurance', amount: 294.76, date: new Date(2021, 2, 28)},
+                   {title: 'life insurance', amount: 295.76, date: new Date(2021, 3, 28)}]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+      title={expenses[0].title}
+      amount ={expenses[0].amount}
+      date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem
+      title={expenses[1].title}
+      amount ={expenses[1].amount}
+      date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
-}
+  }
 
 export default App;
