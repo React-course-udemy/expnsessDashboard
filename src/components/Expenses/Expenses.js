f470359107e93dd07a4a8b403b00a249fc161971
thepassant <bassant.ayman69@gmail.com>
2022-01-12 00:37:31 +0200

    use expenses in state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,42 +1,31 @@
-import{useState} from "react";
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "../../css/Expenses.css";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = ({ list }) => {
-  const [filteredData, setFilteredData] = useState('2020');
- 
-    const onSelectYearHandler = (year) => {
-        setFilteredData(year);
-        console.log(year);
-    }
+  const [filteredData, setFilteredData] = useState("2020");
+
+  const onSelectYearHandler = (year) => {
+    setFilteredData(year);
+    console.log(year);
+  };
   return (
-    
-      
-      <Card classes="expenses">
-      <ExpenseFilter selected={filteredData} onSelectYear = {onSelectYearHandler}/> 
+    <Card classes="expenses">
+      <ExpenseFilter
+        selected={filteredData}
+        onSelectYear={onSelectYearHandler}
+      />
+      {list.map((expense) => (
         <ExpenseItem
-          title={list[0].title}
-          amount={list[0].amount}
-          date={list[0].date}
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
         />
-        <ExpenseItem
-          title={list[1].title}
-          amount={list[1].amount}
-          date={list[1].date}
-        />
-        <ExpenseItem
-          title={list[2].title}
-          amount={list[2].amount}
-          date={list[2].date}
-        />
-        <ExpenseItem
-          title={list[3].title}
-          amount={list[3].amount}
-          date={list[3].date}
-        />
-      </Card>
+      ))}
+    </Card>
   );
 };
 
