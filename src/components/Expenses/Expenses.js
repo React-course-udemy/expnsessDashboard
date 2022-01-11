144e8c3e70b312a5789d83afd3c620dee54a0c58
thepassant <bassant.ayman69@gmail.com>
2022-01-11 12:11:50 +0200

    add expense filtre by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,42 @@
+import{useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import "../../css/Expenses.css";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = ({ list }) => {
+  const [filteredData, setFilteredData] = useState('2020');
+ 
+    const onSelectYearHandler = (year) => {
+        setFilteredData(year);
+        console.log(year);
+    }
   return (
-    <Card classes="expenses">
-      <ExpenseItem
-        title={list[0].title}
-        amount={list[0].amount}
-        date={list[0].date}
-      />
-      <ExpenseItem
-        title={list[1].title}
-        amount={list[1].amount}
-        date={list[1].date}
-      />
-      <ExpenseItem
-        title={list[2].title}
-        amount={list[2].amount}
-        date={list[2].date}
-      />
-      <ExpenseItem
-        title={list[3].title}
-        amount={list[3].amount}
-        date={list[3].date}
-      />
-    </Card>
     
+      
+      <Card classes="expenses">
+      <ExpenseFilter selected={filteredData} onSelectYear = {onSelectYearHandler}/> 
+        <ExpenseItem
+          title={list[0].title}
+          amount={list[0].amount}
+          date={list[0].date}
+        />
+        <ExpenseItem
+          title={list[1].title}
+          amount={list[1].amount}
+          date={list[1].date}
+        />
+        <ExpenseItem
+          title={list[2].title}
+          amount={list[2].amount}
+          date={list[2].date}
+        />
+        <ExpenseItem
+          title={list[3].title}
+          amount={list[3].amount}
+          date={list[3].date}
+        />
+      </Card>
   );
 };
 
